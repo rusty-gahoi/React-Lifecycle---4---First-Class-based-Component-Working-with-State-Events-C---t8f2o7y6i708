1433d9453bb68762997e15b514cea0d68ddddea7
rusty-gahoi <vipulrocks28@gmail.com>
2022-12-12 00:44:59 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,9 @@ class App extends React.Component{
     this.handleClick = this.handleClick.bind(this)
   }
 
-
+  shouldComponentUpdate(nextProps,nextState){
+    return nextState.count % 2 === 0
+  }
   render(){
     console.log(`Rendering with count:-${this.state.count}`)
     return(
